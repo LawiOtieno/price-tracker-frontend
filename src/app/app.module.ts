a084d59c2c727f8498009af317ae6c6fceed8f0a
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-11-04 11:24:57 +0300

    create structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    LogoutComponent,
+    HomeComponent,
+    ProductsComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
